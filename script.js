2062b47d981476c9451ed950198121f60a4dba39
marcialino <marcialino100@gmail.com>
2023-07-07 18:03:08 -0300

    atualizei

---
+++ script.js
@@ -0,0 +1,6 @@
+function mudaCor () { 
+    document.getElementById('texto').style.color = 'blue'
+    document.getElementById('texto').style.textDecoration = 'underline'
+    document.getElementById('texto').style.fontFamily ='verdana'
+
+ }
